eb389f2a797a8c88d4a35fcd359f2bc8f5ebb9c6
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-13 16:11:44 +0530

    Total Emp Wage

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -2,21 +2,34 @@
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, EMP_RATE_PER_HOUR = 20, NUM_OF_WORKING_DAYS = 20, MAX_HOURS_IN_MONTH = 100;
         static void Main(string[] args)
         {
-            //Constants
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            // Variables
+            int empHrs = 0, totalEmpHrs = 0, totalempwage = 0, totalworkingDays = 0;
+
+            while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalworkingDays < NUM_OF_WORKING_DAYS)
             {
-                Console.WriteLine("Employee is Present");
+                totalworkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days : " + totalworkingDays + " Emp Hours : " + empHrs);
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
+            totalempwage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage : " + totalempwage);
         }
     }
 }
