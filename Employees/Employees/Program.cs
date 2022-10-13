8a2fdd4f5f6c873b5da3d05fcf1ed96f4f94490d
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-13 15:28:28 +0530

    Calculate EmployeeWage Using Switch

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -2,21 +2,31 @@
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            //Constants
-            int IS_FULL_TIME = 1;
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
             {
-                Console.WriteLine("Employee is Present");
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " +empWage);
         }
     }
 }
