3a5dc81759f6f528deb081db5533df1c17fd5161
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-13 15:55:18 +0530

    Wage of Month

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -2,21 +2,33 @@
 {
     class Program
     {
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, EMP_RATE_PER_HOUR = 20, NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
-            //Constants
-            int IS_FULL_TIME = 1;
-            Random random = new Random();
-            //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            // Variables
+        int empHrs = 0, empWage = 0, totalempwage = 0;
+
+            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                Console.WriteLine("Employee is Present");
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
             }
-            else
-            {
-                Console.WriteLine("Employee is absent");
-            }
-        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        totalempwage = empWage * NUM_OF_WORKING_DAYS;
+        Console.WriteLine("Emp Wage : " + empWage);
+        Console.WriteLine("Total Emp Wage : " + totalempwage);
     }
+}
 }
