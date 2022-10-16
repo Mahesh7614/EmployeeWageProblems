2a07e26658e3822d2de971fee007f0a42d254b2f
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-16 22:05:54 +0530

    Using ArrayList

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -7,21 +7,13 @@ namespace Employees
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Company Name : ");
-            string CompName = Console.ReadLine();
-
-            Console.WriteLine("Enter Employee Rate Per Hour : ");
-            int rate = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter No of Working Days : ");
-            int numOfDays = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter Maximum Hours Per Month : ");
-            int maxHourMonth = Convert.ToInt32(Console.ReadLine());
-            
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addcompanyEmpWage(CompName, rate, numOfDays, maxHourMonth);
+            empWageBuilder.addcompanyEmpWage("Airtel",20,25,100);
+            empWageBuilder.addcompanyEmpWage("Tcs",22,26,200);
+            empWageBuilder.addcompanyEmpWage("Mahindra",30,30,110);
+            empWageBuilder.addcompanyEmpWage("Cilpa",15,28,113);
             empWageBuilder.computeEmpWage();
+            
         }
     }
 }
