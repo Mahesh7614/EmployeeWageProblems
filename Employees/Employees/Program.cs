1ab35dffd975cc5e9e97b3d37320aa028a7a9576
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-10-17 12:57:42 +0530

    New Code

--- Employees/Employees/Program.cs
+++ Employees/Employees/Program.cs
@@ -8,12 +8,40 @@ namespace Employees
         static void Main(string[] args)
         {
             EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-            empWageBuilder.addcompanyEmpWage("Airtel",20,25,100);
-            empWageBuilder.addcompanyEmpWage("Tcs",22,26,200);
-            empWageBuilder.addcompanyEmpWage("Mahindra",30,30,110);
-            empWageBuilder.addcompanyEmpWage("Cilpa",15,28,113);
+
+            Console.WriteLine("Enter i to Enter Company Details or otherwise enter any key : ");
+            var input = Console.ReadLine();
+            while (input == "i")
+            {
+                var obj = UserInput();
+                empWageBuilder.addcompanyEmpWage(obj.company, obj.empRatePerHour, obj.numOfWorkingDays, obj.maxHoursPerMonth);
+                Console.WriteLine("Enter i to Enter Company Details or otherwise enter any key :");
+                input = Console.ReadLine();
+            }
             empWageBuilder.computeEmpWage();
-            
+
+        }
+        public static CompanyEmpWage UserInput()
+        {
+            Console.WriteLine("Enter Company Name : ");
+            string compName = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Rate Per Hour : ");
+            int empRatePerHour = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter No of Working Days : ");
+            int numOfDays = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Maximum Hours Per Month : ");
+            int maxWorkingHourPerMonth = Convert.ToInt32(Console.ReadLine());
+
+            return new CompanyEmpWage(compName, empRatePerHour, numOfDays, maxWorkingHourPerMonth)
+            {
+                company = compName,
+                empRatePerHour = empRatePerHour,
+                numOfWorkingDays = numOfDays,
+                maxHoursPerMonth = maxWorkingHourPerMonth
+            };
         }
     }
 }
